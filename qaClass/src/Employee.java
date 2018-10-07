a001335ea4f600a986998ee7ebbf61ecb32b4c4b
priyankabathla <priyankabathla@192.168.0.28>
2018-10-07 15:13:39 -0500

    Adding comments

---
+++ qaClass/src/Employee.java
@@ -0,0 +1,67 @@
+public class Employee {
+
+	int empID;
+	String name;
+	int age;
+	String designation;
+	double salary;
+
+	public static final String company = "A&M Technologies";
+
+	// This is the constructor of the class Employee
+	public Employee(int empID) {
+		this.empID = empID;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// Assign the age of the Employee to the variable age.
+	public void setAge(int empAge) {
+		age = empAge;
+	}
+
+	/* Assign the designation to the variable designation. */
+	public void setDesignation(String empDesig) {
+		designation = empDesig;
+	}
+
+	/* Assign the salary to the variable salary. */
+	public void setSalary(double empSalary) {
+		salary = empSalary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public double netSalary(double sal, String status) {
+		double netsal;
+		if (status == "Married") {
+			netsal = sal - (sal * 0.2);
+		} else {
+			netsal = sal - (sal * 0.3);
+		}
+		return netsal;
+	}
+	
+	public double bonus() {
+		return 100;
+	}
+
+	
+
+}
